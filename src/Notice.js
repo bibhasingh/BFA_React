197946b698074568f51b3510967808f474c0aa47
Bibha Singh <bibhaanil@gmail.com>
2025-05-25 20:59:17 +0200

    notice

--- src/Notice.js
+++ src/Notice.js
@@ -1,10 +1,18 @@
 import { useContent } from "./Content";
+import { useMyLang } from "./Lang";
 import "./notice.css";
 
 
 
 function Notice() {
     const { updateContent } = useContent();
+    const { myCurrentLang } = useMyLang();
+
+    const registration={
+        Fr: "Inscrivez-vous pour la session 2025-2026",
+        Nl: "Registreer voor sessie 2025-2026",
+        En: "Register for session 2025-2026",
+    }
 
     function handleNoticeClick() {
         console.log('abc');
@@ -18,7 +26,7 @@ function Notice() {
                 className="notice-link"
                 onClick={handleNoticeClick}
             >
-                🚀 Register for session 2025-2026!
+                🚀 {registration[myCurrentLang]}!
             </button>
         </div>
     );
